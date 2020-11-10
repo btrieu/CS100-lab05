c2c5bcfd4c410d34e7bf19e76350d0cf885ddf47
btrieu <counspls@gmail.com>
2020-11-10 14:35:23 -0800

    Implemented rectangle.cpp

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,25 +1,45 @@
 #include "../header/rectangle.hpp"
 
 Rectangle::Rectangle() {
-	
+        this->width = 0;
+        this->height = 0;
 }
 
 Rectangle::Rectangle(int w, int h) {
-
+        this->width = w;
+        this->height = h;
 }
 
 void Rectangle::set_width(int w) {
-
+        this->width = w;
 }
 
 void Rectangle::set_height(int h) {
-
+        this->height = h;
 }
 
 int Rectangle::area() {
-
+	if(height < 0 && width < 0) {
+		return height*width;
+	}
+	else if (height < 0) {
+		return height*width*-1;
+	}
+	else if (width < 0) {
+		return height*width*-1;
+	}
+	return height*width;
 }
 
 int Rectangle::perimeter() {
-
+	int modHeight = height;
+	int modWidth = width;
+	if(height < 0) {
+		modHeight = height *-1;
+	}
+	if(width < 0) {
+		modWidth = width *-1;
+	}
+        return modHeight+modHeight+modWidth+modWidth;
 }
+
