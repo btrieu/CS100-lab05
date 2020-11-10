2b29819d93c677a3118e2c3c2cf05b7b932c538a
btrieu <57569278+btrieu@users.noreply.github.com>
2020-11-10 13:41:53 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main() {
 	Rectangle rect1 = Rectangle(4, 4);
 	cout << "area: " << rect1.area() << endl;
-	cout << "perimeter: " << rec1.perimeter() << endl;
+	cout << "perimeter: " << rect1.perimeter() << endl;
 	return 0;
 }
